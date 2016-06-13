2dc7045147a306aa8109865334b33f9dd0f40aa1
whiteUnicorn <bdh931101@gmail.com>
2016-06-14 02:02:26 +0900

    Delete useless code

--- _site/install-mac.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install ruby
-sudo gem install github-pages
-echo "You can run the server by using 'jekyll serve'."
